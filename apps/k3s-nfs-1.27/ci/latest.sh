ed521e8c04f717e369971ff015691d783412bd88
Ryan-McD <72583999+Ryan-McD@users.noreply.github.com>
2023-07-08 21:31:03 -0400

    feat: add k3s 1.27 and stable channel

---
+++ apps/k3s-nfs-1.27/ci/latest.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+version="$(curl -s https://update.k3s.io/v1-release/channels | jq '.data[] | select(.id=="v1.27")' | jq -r .latest 2>/dev/null | sed "s/+/-/g")"
+printf "%s" "${version}"
